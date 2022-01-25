d9af9aae880ba66dd8fbff56d97d4f68d42dbc42
Nick Allen <rutherford.t.b@gmail.com>
2022-01-25 14:38:10 -0600

    add icons

--- src/components/singleTodo.tsx
+++ src/components/singleTodo.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Todo } from '../model';
+import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
+import { MdDone } from 'react-icons/md';
 
 type Props = {
     todo: Todo,
@@ -13,6 +15,17 @@ const SingleTodo = ({todo, todos, setTodos} : Props) => {
       <span className="todos__single--text">
           {todo.todo}
       </span>
+      <div>
+          <span className="icon">
+              <AiFillEdit />
+          </span>
+          <span className="icon">
+              <AiFillDelete/>
+          </span>
+          <span className="icon">
+              <MdDone />
+          </span>
+      </div>
   </form>;
 };
 
